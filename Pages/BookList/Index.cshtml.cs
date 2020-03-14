175053796939771122bb3b6f1deea9b6201aa063
rajat1994 <rajatarora21@gmail.com>
2020-03-14 22:13:35 +0530

    Delete Func Done

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -27,5 +27,24 @@ namespace BookListRazor
 
 
         }
+
+
+         public async Task<IActionResult> OnPostDelete(int id)
+        {
+
+            var book = await _db.Book.FindAsync(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
+
     }
 }
